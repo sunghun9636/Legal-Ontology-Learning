462e3949a3bdc8a7c6140708bfbd1c26f8644050
sunghun9636 <sunghunjung96@gmail.com>
2019-02-22 23:11:32 +0000

    trying simple LDA topic modelling

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,6 +1,7 @@
 from os.path import join
 import pandas as pd
 from preprocess import normalize, text_to_tokens
+import pickle
 
 
 def data_load(data_path): # load json file data into dataframe
@@ -15,19 +16,26 @@ def data_load(data_path): # load json file data into dataframe
 def load_and_preprocess(data_path):
     # loading the data from json into df
     data = data_load(data_path)
+    output = []
 
     # preprocessing the text
-    for i in range(len(data["casebody"])):
+    # for i in range(len(data["casebody"])):
+    for i in range(100):
         data["casebody"][i]["data"] = normalize(text_to_tokens(data["casebody"][i]["data"]))
-        # break
+        output.append(data["casebody"][i]["data"])
 
-    return data
+    with open('case_documents.data', 'wb') as file:
+        print("... Saving the pre-processed data into local binary file...")
+        # store the data as binary data stream
+        pickle.dump(output, file)
+
+    return output
 
 
 def main():
     path = join("data", "Arkansas-20181204-xml", "data", "data.json") # path to the data
 
-    print(load_and_preprocess(path)["casebody"][0]["data"])
+    print(load_and_preprocess(path)[0])
 
 
 if __name__ == '__main__':
---
+++ src/topic_model.py
@@ -0,0 +1,31 @@
+import gensim.models as models
+import pickle
+from data_preparation import remove_low_frequent_words, get_tfidf
+
+
+def train_lda_model(num_topics):
+    with open('case_documents.data', 'rb') as file:
+        # read the data as binary data stream
+        print("... reading the pre-processed data from local binary file...")
+        documents = pickle.load(file)
+
+    documents = remove_low_frequent_words(documents)
+
+    lda_model = models.ldamodel.LdaModel(corpus=get_tfidf(documents)['corpus_tfidf'],
+                                         id2word=get_tfidf(documents)['index2word'],
+                                         num_topics=num_topics,
+                                         random_state=100,
+                                         update_every=1,
+                                         chunksize=100,
+                                         passes=10,
+                                         alpha='auto',
+                                         per_word_topics=True)
+    return lda_model
+
+
+def main():
+    print(train_lda_model(5).print_topics())
+
+
+if __name__ == '__main__':
+    main()

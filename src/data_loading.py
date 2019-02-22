1033596434aaac60b75bbe8a317cd39e53c30c60
sunghun9636 <sunghunjung96@gmail.com>
2019-02-22 16:39:13 +0000

    replacing NE with the type name

--- src/data_loading.py
+++ src/data_loading.py
@@ -2,8 +2,8 @@ from os.path import join
 import pandas as pd
 from preprocess import normalize, text_to_tokens, replace_ner
 
-# load json file data into dataframe
-def data_load(data_path):
+
+def data_load(data_path): # load json file data into dataframe
     with open(data_path) as data_file:
         df = pd.read_json(data_file, lines=True) # read each line of the file as JSON item
 
@@ -22,6 +22,7 @@ def load_and_preprocess(data_path):
     # preprocessing the text
     for i in range(len(data["casebody"])):
         data["casebody"][i]["data"] = normalize(text_to_tokens(data["casebody"][i]["data"]))
+        # break
 
     return data
 
@@ -29,10 +30,7 @@ def load_and_preprocess(data_path):
 def main():
     path = join("data", "Arkansas-20181204-xml", "data", "data.json") # path to the data
 
-    # print(load_and_preprocess(path)["casebody"][10]["data"])
-
-    df = data_load(path)
-    replace_ner(df["casebody"][100]["data"])
+    print(load_and_preprocess(path)["casebody"][0]["data"])
 
 
 if __name__ == '__main__':
--- src/preprocess.py
+++ src/preprocess.py
@@ -18,13 +18,8 @@ def replace_contractions(text):
     return contractions.fix(text)
 
 
-def replace_ner(text):
-    # nlp = spacy.load('en')
+def replace_ner(text): # function to replace named entity with their category e.g. PERSON
     nlp_big = spacy.load('en_core_web_md')
-
-    # entities_big = [e.string for e in document_big.ents if 'PERSON' == e.label_]
-    # entities_big = list(entities_big)
-    # print(entities_big)
 
     doc = nlp_big(text)
     redacted_sentence = []
@@ -32,14 +27,14 @@ def replace_ner(text):
     for ent in doc.ents:
         ent.merge()
 
-    for token in doc:
-        if token.ent_type_ == "PERSON":
-            redacted_sentence.append("PERSON")
-        else:
+    for token in doc: # if the token is NE, replace with the type, otherwise leave it as it is
+        if token.ent_type_ == "":
             redacted_sentence.append(token.string)
+        else:
+            redacted_sentence.append(token.ent_type_)
 
-    print(redacted_sentence)
-    return "".join(redacted_sentence)
+    # return "".join(redacted_sentence)
+    return redacted_sentence
 
 
 def tokenize(text):
@@ -47,7 +42,7 @@ def tokenize(text):
 
 
 def text_to_tokens(text):
-    return tokenize(replace_contractions(text))
+    return replace_ner(replace_contractions(text))
 
 
 ######################### Dealing with tokens, not text from here ################################

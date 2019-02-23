b746fe43f3232440a1fbea69d0edd93bfb88b641
sunghun9636 <sunghunjung96@gmail.com>
2019-02-23 19:09:15 +0000

    adding simple svd model

--- src/topic_model.py
+++ src/topic_model.py
@@ -23,8 +23,26 @@ def train_lda_model(num_topics):
     return lda_model
 
 
+def train_svd_model(num_topics):
+    with open('case_documents.data', 'rb') as file:
+        # read the data as binary data stream
+        print("... Reading the pre-processed data from local binary file...")
+        documents = pickle.load(file)
+
+    documents = remove_low_frequent_words(documents, 0.10)
+
+    svd_model = models.LsiModel(corpus=get_tfidf(documents)['corpus_tfidf'],
+                                id2word=get_tfidf(documents)['index2word'],
+                                num_topics=num_topics)
+
+    return svd_model
+
+
 def main():
+    print("RESULT FROM LDA: ")
     print(train_lda_model(10).print_topics())
+    print("RESULT FROM SVD: ")
+    print(train_svd_model(10).print_topics())
 
 
 if __name__ == '__main__':

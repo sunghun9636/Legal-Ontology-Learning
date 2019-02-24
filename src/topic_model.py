a07d2e5e51da2f09fe64139c8ee51c8dd3fa2ca7
sunghun9636 <sunghunjung96@gmail.com>
2019-02-24 15:19:07 +0000

    debugging

--- src/topic_model.py
+++ src/topic_model.py
@@ -3,13 +3,13 @@ import pickle
 from data_preparation import remove_low_high_frequent_words, get_tfidf
 
 
-def train_lda_model(num_topics):
-    with open('case_documents_500.data', 'rb') as file:
+def train_lda_model(data, num_topics):
+    with open(data, 'rb') as file:
         # read the data as binary data stream
         print("... Reading the pre-processed data from local binary file...")
         documents = pickle.load(file)
 
-    documents = remove_low_high_frequent_words(documents, 0.1, 0.7)
+    documents = remove_low_high_frequent_words(documents, 0.25, 0.80)
 
     lda_model = models.ldamodel.LdaModel(corpus=get_tfidf(documents)['corpus_tfidf'],
                                          id2word=get_tfidf(documents)['index2word'],
@@ -23,13 +23,13 @@ def train_lda_model(num_topics):
     return lda_model
 
 
-def train_svd_model(num_topics):
-    with open('case_documents_10.data', 'rb') as file:
+def train_svd_model(data, num_topics):
+    with open(data, 'rb') as file:
         # read the data as binary data stream
         print("... Reading the pre-processed data from local binary file...")
         documents = pickle.load(file)
 
-    documents = remove_low_high_frequent_words(documents, 0.1, 0.7)
+    documents = remove_low_high_frequent_words(documents, 0.25, 0.80)
 
     svd_model = models.LsiModel(corpus=get_tfidf(documents)['corpus_tfidf'],
                                 id2word=get_tfidf(documents)['index2word'],
@@ -40,9 +40,9 @@ def train_svd_model(num_topics):
 
 def main():
     print("RESULT FROM LDA: ")
-    print(train_lda_model(5).print_topics())
+    print(train_lda_model('data/case_documents_1000.data', 20).print_topics())
     print("RESULT FROM SVD: ")
-    print(train_svd_model(5).print_topics())
+    print(train_svd_model('data/case_documents_1000.data', 20).print_topics())
 
 
 if __name__ == '__main__':

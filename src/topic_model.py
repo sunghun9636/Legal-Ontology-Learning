bb12b3139d6515ba31a297a90cb29bc00f3ee66a
sunghun9636 <sunghunjung96@gmail.com>
2019-02-26 17:34:59 +0000

    refactoring topic modelling file

--- src/topic_model.py
+++ src/topic_model.py
@@ -10,14 +10,17 @@ def train_lda_model(data, num_topics):
         print("... Reading the pre-processed data from local binary file...")
         documents = pickle.load(file)
 
-    documents = remove_low_high_frequent_words(documents, 0.10, 0.30)
+    documents = remove_low_high_frequent_words(documents, 0.15, 0.60)
 
-    lda_model = models.ldamodel.LdaModel(corpus=get_tfidf(documents)['corpus_tfidf'],
-                                         id2word=get_tfidf(documents)['index2word'],
+    corpus = get_tfidf(documents)["corpus_tfidf"]
+    in2word = get_tfidf(documents)["index2word"]
+
+    lda_model = models.ldamodel.LdaModel(corpus=corpus,
+                                         id2word=in2word,
                                          num_topics=num_topics,
                                          passes=10)
 
-    return lda_model, get_tfidf(documents)['corpus_tfidf'], get_tfidf(documents)['index2word']
+    return lda_model, corpus, in2word
 
 
 def train_svd_model(data, num_topics):
@@ -26,33 +29,38 @@ def train_svd_model(data, num_topics):
         print("... Reading the pre-processed data from local binary file...")
         documents = pickle.load(file)
 
-    documents = remove_low_high_frequent_words(documents, 0.10, 0.50)
+    documents = remove_low_high_frequent_words(documents, 0.15, 0.60)
 
-    svd_model = models.LsiModel(corpus=get_tfidf(documents)['corpus_tfidf'],
-                                id2word=get_tfidf(documents)['index2word'],
+    corpus = get_tfidf(documents)["corpus_tfidf"]
+    in2word = get_tfidf(documents)["index2word"]
+
+    svd_model = models.LsiModel(corpus=corpus,
+                                id2word=in2word,
                                 num_topics=num_topics,
                                 chunksize=5,
                                 onepass=False,
                                 power_iters=10)
 
-    return svd_model
+    return svd_model, corpus, in2word
 
 
-def lda_visualization():
-    model, corpus, dictionary = train_lda_model('data/case_documents_1000.data', 10)
+def lda_visualization(data, num_topics):
+    model, corpus, dictionary = train_lda_model(data, num_topics)
+    print("length of the dictionary is: ")
+    print(len(dictionary))
 
     visual = pyLDAvis.gensim.prepare(model, corpus, dictionary)
-    pyLDAvis.save_html(visual, 'visual.html')
+    pyLDAvis.save_html(visual, 'visual/lda_visual.html')
 
 
 def main():
     # print("RESULT FROM LDA: ")
-    # print(train_lda_model('data/case_documents_100.data', 10).print_topics())
+    # print(train_lda_model('data/case_documents_1000.data', 10).print_topics())
     # print("RESULT FROM SVD: ")
-    # print(train_svd_model('data/case_documents_100.data', 10).print_topics())
+    # print(train_svd_model('data/case_documents_1000.data', 10).print_topics())
 
-    # visualization
-    lda_visualization()
+    # LDA visualization
+    lda_visualization('data/case_documents_1000.data', 10)
 
 
 if __name__ == '__main__':

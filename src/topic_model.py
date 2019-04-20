6c06583bbc0d25467ad63ee42936b6c9aa01e439
sunghun9636 <sunghunjung96@gmail.com>
2019-04-20 19:12:51 +0100

    coherence score for number of topics decision making

--- src/topic_model.py
+++ src/topic_model.py
@@ -3,6 +3,7 @@ import pickle
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from gensim.models import CoherenceModel
 from data_preparation import remove_low_high_frequent_words, get_tfidf, extract_important_words_tfidf
 import pyLDAvis.gensim
 
@@ -27,7 +28,7 @@ def train_lda_model(data, num_topics):
                                          passes=1,  # default: 1
                                          update_every=1,  # default: 1
                                          alpha='symmetric',  # default: 'symmetric'
-                                         eta=0.3,  # default: None
+                                         eta=0.3,  # default: None ; ** taking non-default value **
                                          decay=0.5,  # default: 0.5
                                          offset=1.0,  # default: 1.0
                                          eval_every=10,  # default: 10
@@ -91,18 +92,61 @@ def plot_word_importance(model):
     plt.show()
 
 
-def main():
-    # print("RESULT FROM LDA: ")
-    # print(train_lda_model('data/case_documents_1000.data', 10).print_topics())
-    # print("RESULT FROM SVD: ")
-    # print(train_svd_model('data/case_documents_1000.data', 10).print_topics())
+def compute_coherence_values_topic_num(data, limit, start=2, step=1):
+    """
+    Compute u_mass coherence for various number of topics
 
+    Returns:
+    -------
+    model_list : List of LDA topic models
+    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+    """
+    with open(data, 'rb') as file:
+        # read the data as binary data stream
+        print("... Reading the pre-processed data from local binary file...")
+        documents = pickle.load(file)
+
+    documents = extract_important_words_tfidf(documents, 0.60)  # extracting top 60% (TF-IDF) terms per document
+    documents = remove_low_high_frequent_words(documents, 0.03, 1.0)
+
+    corpus = get_tfidf(documents)["corpus_tfidf"]
+    dictionary = get_tfidf(documents)["index2word"]
+
+    coherence_values = []
+    model_list = []
+
+    for num_topics in range(start, limit, step):
+        model = models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics, eta=0.3)
+        model_list.append(model)
+
+        coherencemodel = CoherenceModel(model=model, dictionary=dictionary, corpus=corpus, coherence='u_mass')
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
+
+
+def main():
     # ------------- LDA visualization ---------------- #
-    lda_visualization('data/case_documents_20000.data', 10)
+    lda_visualization('data/case_documents_20000.data', 5)
 
     # ------------- LDA word importance visualization ---------------- #
     # lda_model = train_lda_model('data/case_documents_5000.data', 10)[0]
     # plot_word_importance(lda_model)
+
+    # ------------- LDA evaluation ---------------- #
+    # model_list, coherence_values = compute_coherence_values_topic_num(data='data/case_documents_20000.data',
+    #                                                                   limit=15,
+    #                                                                   start=2,
+    #                                                                   step=1)
+    # limit = 15
+    # start = 2
+    # step = 1
+    # x = range(start, limit, step)
+    # plt.plot(x, coherence_values)
+    # plt.xlabel("Num Topics")
+    # plt.ylabel("Coherence score")
+    # plt.legend("coherence_values", loc='best')
+    # plt.show()
 
 
 if __name__ == '__main__':

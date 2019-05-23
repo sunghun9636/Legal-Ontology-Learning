d6968b9fc4fe4efb6d8e8e5a784c4b6854281318
sunghun9636 <sunghunjung96@gmail.com>
2019-05-23 18:28:38 +0100

    coherence measure added

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,10 +1,13 @@
 import pickle
 import random
+import numpy as np
 import gensim.models as models
+import matplotlib.pyplot as plt
+from gensim.models import CoherenceModel
 from data_preparation import remove_low_high_frequent_words, get_tfidf, extract_important_words_tfidf
 
 
-def perplexity(data):
+def perplexity(data, limit, start=2, step=1):
     with open(data, 'rb') as file:
         # read the data as binary data stream
         print("... Reading the pre-processed data from local binary file...")
@@ -20,15 +23,75 @@ def perplexity(data):
     train = corpus[:15000]
     test = corpus[15000:]
 
-    model = models.ldamodel.LdaModel(corpus=train, id2word=dictionary, num_topics=5, eta=0.3)
+    perplexity_values = []
+    model_list = []
 
-    perplexity_score = model.log_perplexity(test)
+    for num_topics in range(start, limit, step):
+        model = models.ldamodel.LdaModel(corpus=train, id2word=dictionary, num_topics=num_topics, eta=0.3)
+        model_list.append(model)
 
-    print('{} {}'.format('Perplexity score is: ', perplexity_score))
+        perplexity_values.append(model.bound(test))
+
+    # model = models.ldamodel.LdaModel(corpus=train, id2word=dictionary, num_topics=5, eta=0.3)
+    #
+    # perplexity_score = model.log_perplexity(test)
+    #
+    # print('{} {}'.format('Perplexity score is: ', perplexity_score))
+
+    return model_list, perplexity_values
+
+
+def coherence(data, limit, start=2, step=1):
+    with open(data, 'rb') as file:
+        # read the data as binary data stream
+        print("... Reading the pre-processed data from local binary file...")
+        documents = pickle.load(file)
+
+    documents = extract_important_words_tfidf(documents, 0.60)  # extracting top 60% (TF-IDF) terms per document
+    documents = remove_low_high_frequent_words(documents, 0.03, 1.0)
+
+    corpus = get_tfidf(documents)["corpus_tfidf"]
+    dictionary = get_tfidf(documents)["index2word"]
+
+    # random.shuffle(corpus)
+    train = corpus[:15000]
+    test = corpus[15000:]
+
+    coherence_values = []
+    model_list = []
+
+    for num_topics in range(start, limit, step):
+        model = models.ldamodel.LdaModel(corpus=train, id2word=dictionary, num_topics=num_topics, eta=0.3)
+        model_list.append(model)
+
+        coherencemodel = CoherenceModel(model=model, dictionary=dictionary, corpus=test, coherence='u_mass')
+        coherence_values.append(coherencemodel.get_coherence())
+
+    return model_list, coherence_values
 
 
 def main():
-    perplexity('data/case_documents_20000.data')
+    model_list, coherence_values = coherence(data='data/case_documents_20000.data', limit=15, start=2, step=1)
+    limit = 15
+    start = 2
+    step = 1
+    x = range(start, limit, step)
+    plt.plot(x, coherence_values)
+    plt.xlabel("Num Topics")
+    plt.ylabel("Coherence score")
+    plt.legend("coherence_values", loc='best')
+    plt.show()
+
+    # model_list, perplexity_values = perplexity(data='data/case_documents_20000.data', limit=15, start=2, step=1)
+    # limit = 15
+    # start = 2
+    # step = 1
+    # x = range(start, limit, step)
+    # plt.plot(x, perplexity_values)
+    # plt.xlabel("Num Topics")
+    # plt.ylabel("Perplexity Score")
+    # plt.legend("Perplexity", loc='best')
+    # plt.show()
 
 
 if __name__ == '__main__':
